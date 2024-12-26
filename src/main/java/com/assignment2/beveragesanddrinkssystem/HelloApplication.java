93d934af5368c8b7c34492c872909e0d8fb69317
ryanJackman05 <20108256@mail.wit.ie>
2024-12-26 16:49:22 +0000

    Ryan - small update

--- src/main/java/com/assignment2/beveragesanddrinkssystem/HelloApplication.java
+++ src/main/java/com/assignment2/beveragesanddrinkssystem/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
